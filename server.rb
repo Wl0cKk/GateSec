4546390b53ea1c5686d8f6db2fbe4846f8a47d45
Wl0cKk <kandraciuk404@gmail.com>
2024-07-17 16:12:59 +0300

    Done server logic

--- server.rb
+++ server.rb
@@ -2,11 +2,20 @@ require 'sinatra'
 require 'sqlite3'
 require_relative 'config.rb'
 
+db = SQLite3::Database.new 'sec.db'
+
+def allow_access?(db, id, lvl)
+    query = "SELECT COUNT(*) FROM staff WHERE card_id = ? AND access_lvl >= ?"
+    # access is only possible if the level of the place is lower than or equal to the level of the entering person
+    count = db.get_first_value(query, id, lvl)
+    return count > 0
+end
+
 before do
     content_type :json
 end
 
-get '/:rfid' do |n|
-    { access: "false" }.to_json
+get '/auth' do
+    { access: allow_access?(db, params['id'], params['lvl']) }.to_json
 end
-  
+
