b257cd8b208b7c4fb767d378c05717553a9b0c73
Wl0cKk <kandraciuk404@gmail.com>
2024-07-19 00:13:30 +0300

    This seems not working with MFRC latest version, but 1.2.1 is ok

---
+++ validator_example/config.h
@@ -0,0 +1,13 @@
+#define RED_LED 32
+#define BLUE_LED 12
+#define BUZZER_PIN 2
+#define SS_PIN 21
+#define RST_PIN 22
+
+#define SSID "YOUR WIFI NAME"
+#define PASSWORD "YOUR WIFI PASSWORD"
+
+const char* serverAddress = "SERVER ADDRESS SAME AS IN config.rb";
+const int serverPort = 8000;
+
+#define LEVEL 5
--- validator_example/validator.ino
+++ validator_example/validator.ino
@@ -1,9 +1,100 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <MFRC522.h>
+#include "config.h"
 
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+
+    connectToWiFi();
+
+    SPI.begin();
+    mfrc522.PCD_Init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if (checkRFIDCard()) {
+        String rfidData = readRFIDData();
+        if (!rfidData.isEmpty()) {
+            if (sendRequestAndProcessResponse(rfidData)) {
+                digitalWrite(BLUE_LED, HIGH);
+                digitalWrite(RED_LED, LOW);
+                tone(BUZZER_PIN, 1000, 1000);
+                delay(5000);
+                digitalWrite(BLUE_LED, LOW);
+            } else {
+                digitalWrite(BLUE_LED, LOW);
+                for (int i = 0; i < 2; i++) {
+                    digitalWrite(RED_LED, HIGH);
+                    tone(BUZZER_PIN, 1000, 150);
+                    delay(250);
+                    noTone(BUZZER_PIN);
+                    digitalWrite(RED_LED, LOW);
+                    delay(250);
+                }
+            }
+        }
+    }
+}
 
+bool checkRFIDCard() {
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        return false;
+    }
+    return mfrc522.PICC_ReadCardSerial();
+}
+
+String readRFIDData() {
+    String rfidData = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        rfidData += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+        rfidData += String(mfrc522.uid.uidByte[i], HEX);
+    }
+    return rfidData;
+}
+
+bool sendRequestAndProcessResponse(String rfidData) {
+    String url = "http://" + String(serverAddress) + ":" + String(serverPort) + "/auth?id=" + rfidData + "&lvl=" + String(LEVEL);
+
+    HTTPClient http;
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("WiFi not connected");
+        return false;
+    }
+
+    http.begin(url);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+        DynamicJsonDocument doc(200);
+        deserializeJson(doc, http.getString());
+        bool access = doc["access"];
+        http.end();
+        if (access) {
+            Serial.println("Access granted!");
+        } else {
+            Serial.println("Access denied!");
+        }
+        return access;
+    } else {
+        Serial.print("HTTP Request failed with error code: ");
+        Serial.println(httpCode);
+        http.end();
+        return false;
+    }
+}
+
+void connectToWiFi() {
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting...");
+    }
+    Serial.println("Connected to WiFi");
 }
