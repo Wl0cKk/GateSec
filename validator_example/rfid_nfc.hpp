46a9c4ad4b928a3059766290af15ef99c8328491
Wl0cKk <kandraciuk404@gmail.com>
2024-07-28 03:05:42 +0300

    Create UDP broadcast, with test script

--- validator_example/rfid_nfc.hpp
+++ validator_example/rfid_nfc.hpp
@@ -1,9 +1,6 @@
 #ifndef RFID_NFC_HPP
 #define RFID_NFC_HPP
 
-#include <SPI.h>
-#include <MFRC522.h>
-
 #define SS_PIN 22
 #define RST_PIN 4
 
@@ -11,12 +8,9 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 String getCardUID() {
     String uidStr = "";
-    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-        for (byte i = 0; i < mfrc522.uid.size; i++) {
-            uidStr += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
-            uidStr += String(mfrc522.uid.uidByte[i], HEX);
-        }
-        mfrc522.PICC_HaltA();
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        uidStr += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+        uidStr += String(mfrc522.uid.uidByte[i], HEX);
     }
     return uidStr;
 }
